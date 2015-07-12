b09d4419150c5315df45cfcd8b002c049926a737
Kyle Robert Hughes <kyle.hughes@outlook.com>
2015-07-12 16:23:16 +0100

    Version 1.2: Add copy text

--- AboutDialog.WPF/Properties/AssemblyInfo.cs
+++ AboutDialog.WPF/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Kyle Hughes 2015")]
 [assembly: ComVisible(false)]
 [assembly: Guid("dbc0a8ef-c16b-472b-99e3-a95331b49931")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
